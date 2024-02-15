8cf825551de255548977123a8ef4dc534f238267
zhaoyu <zhaoyu@desy.de>
2024-02-15 17:22:56 -0500

    Adding a test library

---
+++ test/src/main/java/com/Receiver.java
@@ -0,0 +1,12 @@
+package com;
+
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Receiver {
+    @JmsListener(destination = "order-queue", containerFactory = "warehouseFactory")
+    public void receiveMessage(String order){
+        System.out.println("Order received is: "+ order);
+    }
+}
